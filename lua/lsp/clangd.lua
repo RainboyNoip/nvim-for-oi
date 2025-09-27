193b71321594f4cad3622fd69a5d372455c9825d
rainboy <rainboylvx@qq.com>
2025-09-27 12:56:29 +0800

    use clangd as lsp

---
+++ lua/lsp/clangd.lua
@@ -0,0 +1,19 @@
+-- from :https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#clangd
+return {
+    cmd = { 'clangd' },
+    -- Filetypes to automatically attach to.
+    filetypes = { 'cpp' },
+    -- Sets the "workspace" to the directory where any of these files is found.
+    -- Files that share a root directory will reuse the LSP server connection.
+    -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+    root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac", ".git" },
+
+    capabilities = {
+        offsetEncoding = { "utf-8", "utf-16" },
+        textDocument = {
+            completion = {
+                editsNearCursor = true
+            }
+        }
+    }
+}
