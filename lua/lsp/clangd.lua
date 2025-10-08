1926b165f227b146a8060f95f541886a3e711547
rainboy <rainboylvx@qq.com>
2025-10-08 22:47:09 +0800

    fix: 调整 clangd.lua 配置中的缩进和格式，添加 init_options 以支持自定义初始化参数

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -1,12 +1,19 @@
 -- from :https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#clangd
 return {
-    cmd = { 'clangd'},
+    cmd = { 'clangd' },
     -- Filetypes to automatically attach to.
     filetypes = { 'cpp' },
     -- Sets the "workspace" to the directory where any of these files is found.
     -- Files that share a root directory will reuse the LSP server connection.
     -- Nested lists indicate equal priority, see |vim.lsp.Config|.
     root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac", ".git" },
+
+    init_options = {
+        fallbackFlags = { '--std=c++17' },
+        Diagnostics = {
+            Suppress = { "unused-includes" },
+        },
+    },
 
     capabilities = {
         offsetEncoding = { "utf-8", "utf-16" },
