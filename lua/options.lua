bab96fc3dcfc2cf25ed7eeea3a6d694d7183c192
rainboy <rainboylvx@qq.com>
2025-10-09 21:52:01 +0800

    fix: 调整诊断信息的符号显示，增强视觉识别效果

--- lua/options.lua
+++ lua/options.lua
@@ -66,10 +66,10 @@ vim.diagnostic.config({
     },
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN] = "",
-            [vim.diagnostic.severity.INFO] = "",
-            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+            [vim.diagnostic.severity.HINT] = " ",
         },
         texthl = {
             [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
