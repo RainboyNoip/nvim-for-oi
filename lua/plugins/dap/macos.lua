925b357f8151045f527f5766d321dc4c7c3685f7
rainboy <rainboylvx@qq.com>
2025-10-18 22:48:11 +0800

    fix: 添加 codelldb 配置中的终端窗口命令和标准输入重定向，优化调试体验

--- lua/plugins/dap/macos.lua
+++ lua/plugins/dap/macos.lua
@@ -8,6 +8,7 @@ return {
         config = function()
             local dap = require('dap')
             local config_path = vim.fs.dirname(vim.fn.stdpath('config'))
+            dap.defaults.fallback.terminal_win_cmd = "3split new" -- 这里把终端窗口设置的尽可能的小一点,我依然没有找到怎么隐藏这个 termina
             dap.adapters.codelldb = {
                 type = "executable",
                 -- command = "codelldb", -- or if not in $PATH: "/absolute/path/to/codelldb"
@@ -15,6 +16,7 @@ return {
                 name = "lldb",
                 -- On windows you may have to uncomment this:
                 -- detached = false,
+                terminal_win_cmd = "50vsplit new"
             }
             dap.configurations.cpp = {
                 {
@@ -25,12 +27,14 @@ return {
                         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
                     end,
                     cwd = '${workspaceFolder}',
+                    -- console = "internalConsole",
                     stopOnEntry = false, -- 必需是false,不然有汇编代码
-                    -- from here:  https://github.com/vadimcn/codelldb/blob/master/MANUAL.md#stdio-redirection
+
+                    -- 调试的程序输入重定向 from here:  https://github.com/vadimcn/codelldb/blob/master/MANUAL.md#stdio-redirection
                     stdio = function()
                         local input_file = vim.fn.input("Path to read: ", vim.fn.getcwd() .. '/')
-                        return { input_file, 'log.txt', 'tty' }
-                    end,
+                        return { input_file, 'log.txt', 'stderr.log' }
+                    end
                 },
             }
         end,
