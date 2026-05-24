e8a748ff7d1c2cfe7a1973210402c593c5079633
rainboy <rainboylvx@qq.com>
2026-05-24 19:35:13 +0800

    fix: update treesitter config for nvim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,15 +2,23 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
         build = ":TSUpdate",
         config = function()
-            return {
-                ensure_installed = { "cpp" ,"c"},
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                },
-            }
+            require("nvim-treesitter").setup({
+                install_dir = vim.fn.stdpath("data") .. "/site",
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = { "c", "cpp", "markdown" },
+                callback = function(args)
+                    local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+                    if lang then
+                        vim.treesitter.start(args.buf, lang)
+                    end
+                end,
+            })
         end,
     },
 }
